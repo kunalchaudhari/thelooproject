6cdcfaaf7b98338bc5cd50992622c188bb275b6a
Kunal Chaudhari <kunalchaudhari@gmail.com>
2015-04-19 02:02:26 +0530

    fill user's current location in lat and lng field.

--- app/assets/javascripts/thelooproject.js
+++ app/assets/javascripts/thelooproject.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
     details: "#location_section",
     detailsAttribute: "data-geo"
   });
+  initiate_geolocation();
 });
+function initiate_geolocation() {
+  navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+}
+
+function handle_geolocation_query(position){
+  if($('#location_section').length) {
+    $('#toilet_lat').val(position.coords.latitude);
+    $('#toilet_lng').val(position.coords.longitude);
+  }
+}
